5b8ac0a24c4e17811f1da2b66170800cec82d9cf
psharkas <petersharkas7@gmail.com>
2022-07-17 15:55:23 -0400

    gradle + mongo connection

---
+++ gradingmongo/src/main/java/co/grandcircus/gradingmongo/GradingmongoApplication.java
@@ -0,0 +1,13 @@
+package co.grandcircus.gradingmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradingmongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradingmongoApplication.class, args);
+	}
+
+}
---
+++ gradingmongo/src/test/java/co/grandcircus/gradingmongo/GradingmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package co.grandcircus.gradingmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradingmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
